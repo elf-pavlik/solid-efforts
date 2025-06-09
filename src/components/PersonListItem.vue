97fbb7f6b349460d3a02b97252591e27bafbce96
elf-pavlik <elf-pavlik@hackers4peace.net>
2025-06-08 20:24:27 -0600

    loads catalog data

--- src/components/PersonListItem.vue
+++ src/components/PersonListItem.vue
@@ -1,15 +1,16 @@
 <template>
-  <v-list-item
-    :title="props.person.name"
-    :prepend-avatar="props.person.image['@id']"
-  >
+  <v-list-item :title="props.person.name" :prepend-avatar="logo">
   </v-list-item>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
 import { Person } from '@/ldo/shapes.typings';
+import { robohash } from '@/ldo';
 
-  const props = defineProps<{
-    person: Person
-  }>()
+const props = defineProps<{
+  person: Person
+}>()
+
+const logo = computed(() => props.person.logo || robohash(props.person['@id']!))
 </script>
--- src/ldo.ts
+++ src/ldo.ts
@@ -2,11 +2,14 @@ import { useRouter } from "vue-router";
 import { LdoBase, LdoDataset } from "@ldo/ldo";
 import { createLdoDataset } from '@ldo/ldo';
 import { parseJsonld } from '@janeirodigital/interop-utils';
-import { DraftShapeType, ImplementationShapeType, PersonShapeType, ProductClassShapeType, ScopeShapeType } from '@/ldo/shapes.shapeTypes'
-import type { Draft, Implementation, Person, ProductClass, Scope } from '@/ldo/shapes.typings';
-import { data } from '@/data/index'
+import { SpecificationShapeType, ProductShapeType, PersonShapeType, ClassOfProductShapeType } from '@/ldo/shapes.shapeTypes'
+import type { Specification, Product, Person, ClassOfProduct } from '@/ldo/shapes.typings';
 
 export type IRI = { '@id': string }
+
+export function robohash(id: string) {
+  return `https://robohash.org/${encodeURIComponent(id)}.png?set=set3`
+}
 
 export function useLdo() {
 
@@ -17,27 +20,27 @@ export function useLdo() {
       type: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type',
     },
     schema: {
-      Person: 'http://schema.org/Person',
+      Person: 'http://example.org/#Person',
       author: 'http://schema.org/author',
       editor: 'http://schema.org/editor',
     },
     ex: {
-      Primer: 'https://vocab.example/Primer',
-      Specification: 'https://vocab.example/Specification',
-      Draft: 'https://vocab.example/Draft',
-      ProductClass: 'https://vocab.example/ProductClass',
-      Scope: 'https://vocab.example/Scope',
-      Implementation: 'https://vocab.example/Implementation',
-      Application: 'https://vocab.example/Application',
-      Module: 'https://vocab.example/Module',
-      Service: 'https://vocab.example/Service',
-      maintainer: 'https://vocab.example/maintainer',
-      contributor: 'https://vocab.example/contributor',
-      specifies: 'https://vocab.example/specifies',
-      explains: 'https://vocab.example/explains',
-      implements: 'https://vocab.example/implements',
-      accesses: 'https://vocab.example/accesses',
-      dependency: 'https://vocab.example/dependency',
+      Primer: 'http://example.org/#Primer',
+      Specification: 'http://example.org/#Specification',
+      Draft: 'http://example.org/#Draft',
+      ClassOfProduct: 'http://example.org/#ClassOfProduct',
+      Scope: 'http://example.org/#Scope',
+      Product: 'http://example.org/#Product',
+      Application: 'http://example.org/#Application',
+      Module: 'http://example.org/#Module',
+      Service: 'http://example.org/#Service',
+      maintainer: 'http://example.org/#maintainer',
+      contributor: 'http://example.org/#contributor',
+      specifies: 'http://example.org/#specifies',
+      explains: 'http://example.org/#explains',
+      implements: 'http://example.org/#implements',
+      accesses: 'http://example.org/#accesses',
+      dependency: 'http://example.org/#dependency',
     },
     wikidata: {
       typescript: 'http://www.wikidata.org/entity/Q978185',
@@ -57,7 +60,8 @@ export function useLdo() {
 
   async function createDataset(): Promise<void> {
     if (dataset) return
-    const rawDataset = await parseJsonld(JSON.stringify(data))
+    const response = await fetch('https://solidproject.solidcommunity.net/catalog/v2/catalog-data.ttl', { headers: { Accept: 'application/ld+json' } })
+    const rawDataset = await parseJsonld(await response.text())
     dataset = createLdoDataset([...rawDataset])
   }
 
@@ -73,155 +77,158 @@ export function useLdo() {
       .fromSubject(id)
   }
 
-  function getDrafts(): Draft[] {
+  function getDrafts(): Specification[] {
     return dataset
-      .usingType(DraftShapeType)
-      .matchSubject(ns.rdf.type, ns.ex.Draft)
+      .usingType(SpecificationShapeType)
+      .matchSubject(ns.rdf.type, ns.ex.Specification)
   }
 
-  function getDraft(id: string): Draft {
+  function getDraft(id: string): Specification {
     return dataset
-      .usingType(DraftShapeType)
+      .usingType(SpecificationShapeType)
       .fromSubject(id)
   }
 
-  function getProductClasses(): ProductClass[] {
+  function getProductClasses(): ClassOfProduct[] {
     return dataset
-      .usingType(ProductClassShapeType)
-      .matchSubject(ns.rdf.type, ns.ex.ProductClass)
+      .usingType(ClassOfProductShapeType)
+      .matchSubject(ns.rdf.type, ns.ex.ClassOfProduct)
   }
 
-  function getScopes(): Scope[] {
-    return dataset
-      .usingType(ScopeShapeType)
-      .matchSubject(ns.rdf.type, ns.ex.Scope)
+  function getScopes(): any[] { // Scope[] {
+    return []
+    // return dataset
+    //   .usingType(ScopeShapeType)
+    //   .matchSubject(ns.rdf.type, ns.ex.Scope)
   }
 
-  function getScope(id: string): Scope {
+  function getScope(): any { // Scope {
+    return []
+    // return dataset
+    //   .usingType(ScopeShapeType)
+    //   .fromSubject(id)
+  }
+
+  function getProduct(id: string): ClassOfProduct {
     return dataset
-      .usingType(ScopeShapeType)
+      .usingType(ClassOfProductShapeType)
       .fromSubject(id)
   }
 
-  function getProduct(id: string): ProductClass {
+  function getApplications(): Product[] {
     return dataset
-      .usingType(ProductClassShapeType)
-      .fromSubject(id)
-  }
-
-  function getApplications(): Implementation[] {
-    return dataset
-      .usingType(ImplementationShapeType)
+      .usingType(ProductShapeType)
       .matchSubject(ns.rdf.type, ns.ex.Application)
   }
 
-  function getModules(): Implementation[] {
+  function getModules(): Product[] {
     return dataset
-      .usingType(ImplementationShapeType)
+      .usingType(ProductShapeType)
       .matchSubject(ns.rdf.type, ns.ex.Module)
   }
 
-  function getServices(): Implementation[] {
+  function getServices(): Product[] {
     return dataset
-      .usingType(ImplementationShapeType)
+      .usingType(ProductShapeType)
       .matchSubject(ns.rdf.type, ns.ex.Service)
   }
 
-  function getImplementation(id: string): Implementation {
+  function getImplementation(id: string): Product {
     return dataset
-      .usingType(ImplementationShapeType)
+      .usingType(ProductShapeType)
       .fromSubject(id)
   }
 
-  function getDraftsForPerson(id: string): Draft[] {
+  function getDraftsForPerson(id: string): Specification[] {
     const authored = dataset
-      .usingType(DraftShapeType)
+      .usingType(SpecificationShapeType)
       .matchSubject(ns.schema.author, id)
     const edited = dataset
-      .usingType(DraftShapeType)
+      .usingType(SpecificationShapeType)
       .matchSubject(ns.schema.editor, id)
     return [...edited, ...authored].reduce(
       (acc, draft) => acc.find(d => d['@id'] === draft["@id"]) ? acc : [...acc, draft],
-      [] as Draft[]
+      [] as Specification[]
     )
   }
 
-  function getImplementationsForPerson(id: string, type: string): Implementation[] {
+  function getImplementationsForPerson(id: string, type: string): Product[] {
     const maintainedImpl = dataset
-      .usingType(ImplementationShapeType)
+      .usingType(ProductShapeType)
       .matchSubject(ns.ex.maintainer, id)
     // @ts-expect-error
     const maintained = maintainedImpl.filter(i => i.type.find((t: IRI) => t['@id'] === type))
     const contributedImpl = dataset
-      .usingType(ImplementationShapeType)
+      .usingType(ProductShapeType)
       .matchSubject(ns.ex.contributor, id)
     // @ts-expect-error
     const contributed = contributedImpl.filter(i => i.type.find((t: IRI) => t['@id'] === type))
     return [...maintained, ...contributed].reduce(
       (acc, impl) => acc.find(d => d['@id'] === impl["@id"]) ? acc : [...acc, impl],
-      [] as Implementation[]
+      [] as Product[]
     )
   }
 
-  function getApplicationsForPerson(id: string): Implementation[] {
+  function getApplicationsForPerson(id: string): Product[] {
     return getImplementationsForPerson(id, ns.ex.Application)
   }
 
-  function getModulesForPerson(id: string): Implementation[] {
+  function getModulesForPerson(id: string): Product[] {
     return getImplementationsForPerson(id, ns.ex.Module)
   }
-  
-  function getServicesForPerson(id: string): Implementation[] {
+
+  function getServicesForPerson(id: string): Product[] {
     return getImplementationsForPerson(id, ns.ex.Service)
   }
 
-  function getSpecificationForProduct(id: string): Draft {
+  function getSpecificationForProduct(id: string): Specification {
     return dataset
-      .usingType(DraftShapeType)
+      .usingType(SpecificationShapeType)
       .matchSubject(ns.ex.specifies, id)[0]
   }
 
-  function getPrimersForSpecification(id: string): Draft[] {
+  function getPrimersForSpecification(id: string): Specification[] {
     return dataset
-      .usingType(DraftShapeType)
+      .usingType(SpecificationShapeType)
       .matchSubject(ns.ex.explains, id)
   }
 
-  function getImplementationsForProduct(id: string): Implementation[] {
+  function getImplementationsForProduct(id: string): Product[] {
     return dataset
-      .usingType(ImplementationShapeType)
+      .usingType(ProductShapeType)
       .matchSubject(ns.ex.implements, id)
   }
 
-  function getNonModulesForProduct(id: string): Implementation[] {
+  function getNonModulesForProduct(id: string): Product[] {
     const all = dataset
-      .usingType(ImplementationShapeType)
+      .usingType(ProductShapeType)
       .matchSubject(ns.ex.implements, id)
     // @ts-expect-error
     return all.filter(impl => !impl.type.find((t: IRI) => t['@id'] === ns.ex.Module))
   }
 
-  function getModulesForProduct(id: string): Implementation[] {
+  function getModulesForProduct(id: string): Product[] {
     const all = dataset
-      .usingType(ImplementationShapeType)
+      .usingType(ProductShapeType)
       .matchSubject(ns.ex.implements, id)
     // @ts-expect-error
     return all.filter(impl => impl.type.find((t: IRI) => t['@id'] === ns.ex.Module))
   }
 
-  function getApplicationsForModule(id: string): Implementation[] {
+  function getApplicationsForModule(id: string): Product[] {
     return dataset
-      .usingType(ImplementationShapeType)
+      .usingType(ProductShapeType)
       .matchSubject(ns.ex.dependency, id)
   }
 
-  function getApplicationsForScope(id: string): Implementation[] {
+  function getApplicationsForScope(id: string): Product[] {
     return dataset
-      .usingType(ImplementationShapeType)
+      .usingType(ProductShapeType)
       .matchSubject(ns.ex.accesses, id)
   }
 
-  function draftIcon (draft: Draft) {
+  function draftIcon(draft: Specification) {
+    return 'mdi-help-box-outline'
     // @ts-expect-error
     if (draft.type.find((t: IRI) => t['@id'] === ns.ex.Primer)) {
       return 'mdi-information-outline'
@@ -233,31 +240,34 @@ export function useLdo() {
     return 'mdi-help-box-outline'
   }
 
-  function implementationIcon(implementation: Implementation) {
-    switch (implementation.programmingLanguage["@id"]) {
-      case ns.wikidata.typescript:
-        return 'mdi-language-typescript'
-      case ns.wikidata.java:
-        return 'mdi-language-java'
-      case ns.wikidata.rust:
-        return 'mdi-language-rust'
-      default:
-       return 'mdi-help-box-outline'
-    }
+  function implementationIcon(implementation: Product) {
+    return 'mdi-help-box-outline'
+    // switch (implementation.programmingLanguage["@id"]) {
+    //   case ns.wikidata.typescript:
+    //     return 'mdi-language-typescript'
+    //   case ns.wikidata.java:
+    //     return 'mdi-language-java'
+    //   case ns.wikidata.rust:
+    //     return 'mdi-language-rust'
+    //   default:
+    //     return 'mdi-help-box-outline'
+    // }
   }
 
-  function applicationIcon(implementation: Implementation) {
-    if (!implementation.framework) return 'mdi-cellphone'
-    switch (implementation.framework["@id"]) {
-      case 'https://vuejs.org/':
-        return 'mdi-vuejs'
-      default:
-        return 'mdi-cellphone'
-
-    }
+  function applicationIcon(implementation: Product) {
+    return 'mdi-cellphone'
+    // if (!implementation.framework) return 'mdi-cellphone'
+    // switch (implementation.framework["@id"]) {
+    //   case 'https://vuejs.org/':
+    //     return 'mdi-vuejs'
+    //   default:
+    //     return 'mdi-cellphone'
+    //
+    // }
   }
 
-  function scopeIcon(scope: Scope) {
+  function scopeIcon(scope: any/* Scope */) {
+    return 'mdi-flask-empty-outline'
     switch (scope["@id"]) {
       case ns.data.Calendars:
         return 'mdi-calendar-blank'
@@ -275,7 +285,7 @@ export function useLdo() {
     }
   }
 
-  function implementationTypeIcon(implementation: Implementation) {
+  function implementationTypeIcon(implementation: Product) {
     // @ts-expect-error
     if (implementation.type.find((t: IRI) => t['@id'] === ns.ex.Module)) {
       return implementationIcon(implementation)
@@ -290,23 +300,23 @@ export function useLdo() {
     }
     return 'mdi-help-box-outline'
   }
-  
-  function isEditor(draft: Draft, person: Person) {
+
+  function isEditor(draft: Specification, person: Person) {
     // @ts-expect-error
     return draft.editor.find((p: Person) => p['@id'] === person['@id'])
   }
 
-  function isAuthor(draft: Draft, person: Person) {
+  function isAuthor(draft: Specification, person: Person) {
     // @ts-expect-error
     return draft.author.find((p: Person) => p['@id'] === person['@id'])
   }
 
-  function isMaintainer(impl: Implementation, person: Person) {
+  function isMaintainer(impl: Product, person: Person) {
     // @ts-expect-error
     return impl.maintainer.find((p: Person) => p['@id'] === person['@id'])
   }
 
-  function isContributor(impl: Implementation, person: Person) {
+  function isContributor(impl: Product, person: Person) {
     // @ts-expect-error
     return impl.contributor.find((p: Person) => p['@id'] === person['@id'])
   }
@@ -324,13 +334,13 @@ export function useLdo() {
     if (entity.type['@id'] === 'Person' || Array.isArray(entity.type) && entity.type.find((t: IRI) => t['@id'] === 'Person')) {
       name = 'person'
     }
-    if (entity.type['@id'] === 'Draft' || Array.isArray(entity.type) && entity.type.find((t: IRI) => t['@id'] === 'Draft')) {
+    if (entity.type['@id'] === 'Specification' || Array.isArray(entity.type) && entity.type.find((t: IRI) => t['@id'] === 'Specification')) {
       name = 'draft'
     }
-    if (entity.type['@id'] === 'Implementation' || Array.isArray(entity.type) && entity.type.find((t: IRI) => t['@id'] === 'Implementation')) {
+    if (entity.type['@id'] === 'Product' || Array.isArray(entity.type) && entity.type.find((t: IRI) => t['@id'] === 'Product')) {
       name = 'implementation'
     }
-    if (entity.type['@id'] === 'ProductClass' || Array.isArray(entity.type) && entity.type.find((t: IRI) => t['@id'] === 'ProductClass')) {
+    if (entity.type['@id'] === 'ClassOfProduct' || Array.isArray(entity.type) && entity.type.find((t: IRI) => t['@id'] === 'ClassOfProduct')) {
       name = 'product'
     }
     if (entity.type['@id'] === 'Scope' || Array.isArray(entity.type) && entity.type.find((t: IRI) => t['@id'] === 'Scope')) {
@@ -375,6 +385,6 @@ export function useLdo() {
     isContributor,
     getCoverage,
     getConformance,
-    show
+    show,
   }
 }
